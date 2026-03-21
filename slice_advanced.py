9e229bf0e1a3a2d5d71a2e96e484fc4b347b70a2
MartinOrsei <morsei@mail.austral.edu.ar>
2026-03-21 11:45:40 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,4 +2,7 @@ def slice_advanced():
     """Lee un texto e imprime los caracteres desde la posición 4
     en adelante, tomando uno de cada dos (paso 2).
     """
-    pass
+
+    texto= input("Intorduzca un texto:")
+    print(texto[4::2])
+

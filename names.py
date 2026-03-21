a603a648f3b449b6b19840459dac0a93adb1b57f
MartinOrsei <morsei@mail.austral.edu.ar>
2026-03-21 11:41:27 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,14 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+
+    nombre = input("introduzca su nombre: ")
+    apellido = input("introduzca su apellido: ")
+
+    nombre_completo = nombre + " " + apellido
+
+    print(f"{nombre_completo.lower()}")
+    print(f"{nombre_completo.title()}")
+    print(f"{nombre_completo.upper()}")
+    print(f"\t{nombre_completo.lower()}")
+
